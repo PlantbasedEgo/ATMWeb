f9752c0939f1b37513184f31f707f3835311f3c1
estronite <estronite@gmail.com>
2022-09-05 12:43:17 +0700

    V2 Create User/SuperUser

---
+++ user/manager.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+
+class CustomAccountManager(BaseUserManager): #Create Manager for CustomUser
+
+    #-------------------------Base-------------------------------
+    def _create_user(self, username, email, first_name, last_name, password, **extra_fields):
+
+        if not email:
+            raise ValueError("The given username must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    #-----------------------Create Normal User----------------------
+    def create_user(self, username, email, first_name, last_name, password, **extra_fields):
+
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
+        
+    #---------------------Create Super User------------------------------
+    def create_superuser(self, username, email, first_name, last_name, password, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("balance", None)
+        extra_fields.setdefault("withdrawal_fee", None)
+        extra_fields.setdefault("user_id", None)
+        
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
--- user/models.py
+++ user/models.py
@@ -1,45 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from . manager import CustomAccountManager
 
-
-class CustomAccountManager(BaseUserManager): #Create Manager for CustomUser
-
-    #-------------------------Base-------------------------------
-    def _create_user(self, username, email, first_name, last_name, password, **extra_fields):
-
-        if not email:
-            raise ValueError("The given username must be set")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name, **extra_fields)
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    #-----------------------Create Normal User----------------------
-    def create_user(self, username, email, first_name, last_name, password, **extra_fields):
-
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-
-        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
-        
-    #---------------------Create Super User------------------------------
-    def create_superuser(self, username, email, first_name, last_name, password, **extra_fields):
-
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        
-        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
 
 
 # --------------------Customize User---------------------------
@@ -49,9 +13,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_("First Name"), max_length=15)
     last_name = models.CharField(_("Last Name"), max_length=25)
     email = models.EmailField(_("Email Address"), unique=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=5, default=0)
-    withdrawal_fee = models.DecimalField(max_digits=6, decimal_places=6, default=1.0005)
-    user_id = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+    balance = models.DecimalField(max_digits=12, decimal_places=3, default=0, null=True)
+    withdrawal_fee = models.DecimalField(max_digits=6, decimal_places=4, default=1.0005, null=True)
+    user_id = models.DecimalField(max_digits=5, decimal_places=0, default=0, null=True)
     #---------------------------------------------
     about = models.TextField(_("About"), max_length=250, blank=True)
     date_joined = models.DateTimeField(_("Date join"), default=timezone.now)
