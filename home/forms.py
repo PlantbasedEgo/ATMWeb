b859d1c0717d5e14ee0d57903c3bec9e9e51a628
estronite <estronite@gmail.com>
2022-10-03 20:52:11 +0700

    V8

--- home/forms.py
+++ home/forms.py
@@ -2,10 +2,25 @@ from django import forms
 from user.models import CustomUser
 
 
-class DepositForm(forms.ModelForm):
+class DepositForm(forms.Form):
     amount = forms.DecimalField(max_digits=12, decimal_places=3)
+
+class WithdrawForm(forms.ModelForm):
+    amount = forms.DecimalField(max_digits=12, decimal_places=3)
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = CustomUser
-        fields = ['amount']
+        fields = ['amount', 'confirm_password']
 
+    def __init__(self, *args, **kwargs): # *connects with def get_context_data(self, **kwargs):
+        self.user = kwargs.pop('user')
+
+        super(WithdrawForm, self).__init__(*args, **kwargs)
+    
+    def clean(self):
+        cleaned_data = super(WithdrawForm, self).clean()
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if not self.user.check_password(confirm_password):          #check if password is correct before withdraw
+            raise forms.ValidationError("Please reenter your password")
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from user.models import CustomUser
-from . forms import DepositForm
+from . forms import DepositForm, WithdrawForm
 from django.urls import reverse_lazy
 
 class HomePageView(TemplateView):
@@ -14,7 +14,7 @@ class DepositPageView(FormView):
     success_url = reverse_lazy('home_url:home-home')
 
     def form_valid(self, form):
-        deposit_amount = form.cleaned_data['amount']
+        deposit_amount = form.cleaned_data['amount'] 
         self.request.user.balance += deposit_amount
         self.request.user.save()
         return super().form_valid(form)
@@ -25,7 +25,27 @@ class DepositPageView(FormView):
         return context
  
 class WithdrawPageView(FormView):
-    pass
+    model = CustomUser
+    template_name = 'home/withdraw.html'
+    form_class = WithdrawForm
+    success_url = reverse_lazy('home_url:home-home')
+
+
+    def form_valid(self, form):
+        withdraw_amount = form.cleaned_data['amount'] 
+        self.request.user.balance -= withdraw_amount
+        self.request.user.save()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = CustomUser.objects.get(username = self.request.user)
+        return context
+
+    def get_form_kwargs(self):  #*Sending current user to the form, since basic form and ModelForm can't 
+        kwargs = super(WithdrawPageView, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
 
 class TransferPageView(FormView):
     pass
