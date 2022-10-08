7696dd4aabd7f2c57b0cdecd465db5d888cb66fd
estronite <estronite@gmail.com>
2022-10-09 00:07:44 +0700

    V9

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from user.models import CustomUser
-from . forms import DepositForm, WithdrawForm
+from . forms import DepositForm, TransferForm, WithdrawForm
 from django.urls import reverse_lazy
 
 class HomePageView(TemplateView):
@@ -13,39 +13,83 @@ class DepositPageView(FormView):
     form_class = DepositForm
     success_url = reverse_lazy('home_url:home-home')
 
-    def form_valid(self, form):
-        deposit_amount = form.cleaned_data['amount'] 
-        self.request.user.balance += deposit_amount
-        self.request.user.save()
-        return super().form_valid(form)
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context['user'] = CustomUser.objects.get(username = self.request.user)
+
         return context
- 
+
+    def form_valid(self, form):
+        deposit_amount = form.cleaned_data['amount'] 
+
+        self.request.user.balance += deposit_amount
+
+        self.request.user.save()
+        
+        return super().form_valid(form)
+
 class WithdrawPageView(FormView):
     model = CustomUser
     template_name = 'home/withdraw.html'
     form_class = WithdrawForm
     success_url = reverse_lazy('home_url:home-home')
 
-
-    def form_valid(self, form):
-        withdraw_amount = form.cleaned_data['amount'] 
-        self.request.user.balance -= withdraw_amount
-        self.request.user.save()
-        return super().form_valid(form)
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context['user'] = CustomUser.objects.get(username = self.request.user)
+
         return context
 
-    def get_form_kwargs(self):  #*Sending current user to the form, since basic form and ModelForm can't 
-        kwargs = super(WithdrawPageView, self).get_form_kwargs()
+    def form_valid(self, form):
+        withdraw_amount = form.cleaned_data['amount']
+
+        self.request.user.balance -= withdraw_amount
+
+        self.request.user.save()
+
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):  
+        kwargs = super().get_form_kwargs()
         kwargs.update({'user': self.request.user})
         return kwargs
 
 class TransferPageView(FormView):
-    pass
+    model = CustomUser
+    template_name = 'home/transfer.html'
+    form_class = TransferForm
+    success_url = reverse_lazy('home_url:home-home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # retrieve request user from query for writing logic in form_valid
+        context['user'] = CustomUser.objects.get(username = self.request.user)
+        return context
+
+    def form_valid(self, form, **kwargs):
+        # retrieve info from form after validation
+        withdraw_amount = form.cleaned_data['amount'] 
+        receive_username = form.cleaned_data['receive_username']
+        
+        # get the receive_username from cleaned_data and put it into context
+        context = self.get_context_data(**kwargs)
+        context['receive_username'] = CustomUser.objects.get(username = receive_username)
+
+        # get receive_username from query
+        receive_user = CustomUser.objects.get(username = receive_username)
+
+        self.request.user.balance -= withdraw_amount
+        receive_user.balance += withdraw_amount
+
+        # save data of request user
+        self.request.user.save()
+
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):  #Sending user to the form.py, since basic form and ModelForm can't 
+        kwargs = super().get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
